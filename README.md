2ebd7413a18815fecf40eb3d963e3e31612e4ff8
nightna <nightzatree@gmail.com>
2022-02-18 23:50:23 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+server by night
+
+pip install Flask
---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask,request,jsonify, make_response
+from flask_cors import CORS
+import services as services
+
+app = Flask(__name__)
+CORS(app)
+app.config["JSON_AS_ASCII"] = False
+# Create
+@app.route("/api/sensor_data",methods = ["POST", "GET"])
+def sensor_data():
+    # Creat data
+    if request.method == "POST":
+        data = request.get_json()
+        node_id = data['node_id']
+        temp = float(data['temperature'])
+        humi = float(data['humidity'])
+
+        services.save_data(node_id, temp, humi)
+        services.update_node_data(node_id, temp, humi)
+        return make_response(jsonify({"status": "OK"},200))
+    elif request.method == "GET":
+        data = services.get_sensor_data()
+        return make_response(jsonify(data), 200)
+
+# read
+@app.route("/api/node_data")
+def read():
+    data = services.get_node_data()
+    return make_response(jsonify(data),200)
+
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0')
